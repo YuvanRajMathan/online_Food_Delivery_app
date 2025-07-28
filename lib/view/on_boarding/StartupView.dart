5ac27d02ee8e2ddf67730a4320e25e25bd65035d
YuvanRajMathan <myuvanraj020105@gmail.com>
2025-07-28 19:33:24 +0530

    Added primary color and text color

---
+++ lib/view/on_boarding/StartupView.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class StartupView extends StatefulWidget {
+  const StartupView({super.key});
+
+  @override
+  State<StartupView> createState() => _StartupViewState();
+}
+
+class _StartupViewState extends State<StartupView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
